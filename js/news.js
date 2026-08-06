3e2f81f588f8807864c73121c9f0c163cee2f5e7
john okumu <okumujphh@gmail.com>
2026-08-06 16:02:18 +0300

    changed the js for news

--- js/news.js
+++ js/news.js
@@ -3,67 +3,40 @@
 // ===========================
 
 console.log("news.js loaded");
+
 const newsletterForm = document.getElementById("newsletterForm");
-
 const emailInput = document.getElementById("emailInput");
-
 const message = document.getElementById("message");
 
+if (newsletterForm) {
 
-
-if(newsletterForm){
-
-
-    newsletterForm.addEventListener("submit", function(event){
-
+    newsletterForm.addEventListener("submit", function (event) {
 
         // Stop page refresh
         event.preventDefault();
 
+        console.log("Form submitted");
 
+        console.log("Email entered:", emailInput.value);
 
-        const email = emailInput.value.trim();
+        if (emailInput.value === "") {
 
+            message.textContent = "❌ Please enter your email";
 
+        } 
+        else if (!emailInput.checkValidity()) {
 
-        console.log("Email entered:", email);
+            message.textContent = "❌ Please enter a valid email address";
 
+        } 
+        else {
 
+            message.textContent = "✅ Successfully subscribed to Victory FC news!";
 
-        // Email validation pattern
-const newsletterForm = document.getElementById("newsletterForm");
-const emailInput = document.getElementById("emailInput");
-const message = document.getElementById("message");
+            newsletterForm.reset();
 
+        }
 
-newsletterForm.addEventListener("submit", function(event){
+    });
 
-    event.preventDefault();
-
-      console.log("Form submitted");
-
-
-    console.log("Email entered:", emailInput.value);
-
-
-    if(emailInput.value === ""){
-
-        message.textContent = "❌ Please enter your email";
-
-    }
-
-    else if(!emailInput.checkValidity()){
-
-        message.textContent = "❌ Please enter a valid email address";
-
-    }
-
-    else{
-
-        message.textContent = "✅ Successfully subscribed to Victory FC news!";
-
-        newsletterForm.reset();
-
-    }
-
-})
+}
